8fb4ecafac865b30056c6d1654e54bd841e53afd
IIcux <stalker7600@gmail.com>
2018-04-26 16:32:45 +0300

    it's a live

--- src/GCloudPubSubServiceProvider.php
+++ src/GCloudPubSubServiceProvider.php
@@ -3,6 +3,7 @@
 namespace IIcuxoIIaTiK\GCloudPubSub;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
 
 class GCloudPubSubServiceProvider extends ServiceProvider
 {
@@ -10,6 +11,13 @@ class GCloudPubSubServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/gcloud_pubsub.php', 'queue.connections.gcloud_pubsub');
+
+        $this->app['config']->set('queue.connections.gcloud_pubsub.queue_names',
+            Config::get('gcloud_pubsub.queue_names', [])
+        );
+        $this->app['config']->set('queue.connections.gcloud_pubsub.default',
+            Config::get('gcloud_pubsub.default', $this->app['config']->get('queue.connections.gcloud_pubsub', []))
+        );
     }
 
     /**
